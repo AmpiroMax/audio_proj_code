f939291d5b898576d5adf7e310124a1943b3669e
m-kichik <kichik.mg@phystech.edu>
2024-12-16 02:01:52 +0300

    updated metrics

--- src/metrics/metrics.py
+++ src/metrics/metrics.py
@@ -2,13 +2,14 @@ import numpy as np
 import librosa
 from scipy.linalg import sqrtm
 import torch
-torch.manual_seed(42)
 import torch.nn.functional as F
 from transformers import (
     ClapProcessor,
     ClapModel,
     Wav2Vec2Processor,
     Wav2Vec2ForSequenceClassification,
+    AutoProcessor,
+    HubertModel,
 )
 
 
@@ -61,8 +62,8 @@ def evaluate(
     target: dict,
     clap_model,
     clap_processor,
-    wav2vec_model,
-    wav2vec_processor,
+    hubert_model,
+    hubert_processor,
     sr=48000,
     device="cpu",
 ):
@@ -77,7 +78,7 @@ def evaluate(
     """
     clap_model = clap_model.to(device)
 
-    wav2vec_model = wav2vec_model.to(device)
+    hubert_model = hubert_model.to(device)
 
     out_audio, sr = load_audio(out, sr)
     out_inputs = clap_processor(
@@ -97,21 +98,25 @@ def evaluate(
 
     fd = calculate_frechet_distance(out_emb, target_emb)
 
-    out_audio, sr = load_audio(out, sr=16000)  # Wav2Vec requires sr=16kHz
-    out_inputs = wav2vec_processor(
+    out_audio, sr = load_audio(out, sr=16000)  # HuBert requires sr=16kHz
+    out_inputs = hubert_processor(
         audio=out_audio, sampling_rate=sr, return_tensors="pt", padding=True
     )
     out_inputs["input_values"] = out_inputs["input_values"].to(device)
+    out_inputs["attention_mask"] = out_inputs["attention_mask"].to(device)
     with torch.no_grad():
-        out_logs = wav2vec_model(**out_inputs).logits
+        out_logs = hubert_model(**out_inputs).last_hidden_state
+    out_logs = out_logs.mean(dim=1)
 
     target_audio, sr = load_audio(target, sr=16000)
-    target_inputs = wav2vec_processor(
+    target_inputs = hubert_processor(
         audio=target_audio, sampling_rate=sr, return_tensors="pt", padding=True
     )
     target_inputs["input_values"] = target_inputs["input_values"].to(device)
+    target_inputs["attention_mask"] = target_inputs["attention_mask"].to(device)
     with torch.no_grad():
-        target_logs = wav2vec_model(**target_inputs).logits
+        target_logs = hubert_model(**target_inputs).last_hidden_state
+    target_logs = target_logs.mean(dim=1)
 
     kl = calculate_kl_divergence(out_logs, target_logs)
 
@@ -121,11 +126,11 @@ def evaluate(
 def main():
     clap_model = ClapModel.from_pretrained("laion/clap-htsat-unfused")
     clap_processor = ClapProcessor.from_pretrained("laion/clap-htsat-unfused")
+    clap_model.eval()
 
-    wav2vec_model = Wav2Vec2ForSequenceClassification.from_pretrained(
-        "facebook/wav2vec2-base-960h"
-    )
-    wav2vec_processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+    hubert_model = HubertModel.from_pretrained("facebook/hubert-large-ls960-ft")
+    hubert_processor = AutoProcessor.from_pretrained("facebook/hubert-large-ls960-ft")
+    hubert_model.eval()
 
     file1 = "/home/docker_miptml/image2music/audio_proj_code/dataset/music/beethoven_night_10_447.mp3"
     file2 = "/home/docker_miptml/image2music/audio_proj_code/dataset/music/debussi_sea_2_1_404.mp3"
@@ -139,8 +144,8 @@ def main():
             audio2,
             clap_model,
             clap_processor,
-            wav2vec_model,
-            wav2vec_processor,
+            hubert_model,
+            hubert_processor,
             device="cuda:0",
         )
     )
